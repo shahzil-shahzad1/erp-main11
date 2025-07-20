c6f07b4110633a0b232a665acfa4850d5bd4d882
Shahzil Shahzad <silentboys1117@gmail.com>
2025-07-20 20:59:27 +0500

    Update layout.tsx

--- src/app/(commercial)/commercial/layout.tsx
+++ src/app/(commercial)/commercial/layout.tsx
@@ -1,12 +1,8 @@
 'use client'
-import { Navbar } from "@/components/commercial/Navbar";
-import { Sidebar } from "@/components/commercial/Sidebar";
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Navbar />
-      <Sidebar />
       {children}
     </>
   );
